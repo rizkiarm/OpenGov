a17cabe0d76d82c4bf79f7509aefa0c7b818d527
Muhammad Rizki A.R.M <rizki.idea@gmail.com>
2014-05-21 15:49:20 +0700

    Integrate basic maps and add additional dependencies

--- app/app.js
+++ app/app.js
@@ -1,9 +1,17 @@
-var OpenGov = angular.module('OpenGov', ['ngRoute']);
+var OpenGov = angular.module('OpenGov', ['ngRoute','ngMap','ngAnimate']);
 
 OpenGov.config(function($routeProvider){
 	$routeProvider.when('/', {
 		templateUrl : 'app/views/home.html',
 		controller : 'HomeController'
+	});
+	$routeProvider.when('/maps', {
+		templateUrl : 'app/views/maps.html',
+		controller : 'MapController'
+	});
+	$routeProvider.when('/democracy', {
+		templateUrl : 'app/views/democracy.html',
+		controller : 'DemocracyController'
 	});
 	$routeProvider.when('/data/:id/details', {
 		templateUrl : 'app/views/details.html',
--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,6 +1,15 @@
 OpenGov.controller('HomeController', function($scope){
+
 	
 });
+OpenGov.controller('MapController', function($scope){
+	$scope.map = {
+		latlng : '-0.703107, 117.854462'
+	};
+});
 OpenGov.controller('DetailsController', function($scope){
+	
+});
+OpenGov.controller('DemocracyController', function($scope){
 	
 });
