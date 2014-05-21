a52281a67b7751091e3b87fb101c40c587941be2
Muhammad Rizki A.R.M <rizki.idea@gmail.com>
2014-05-21 21:11:32 +0700

    Minor details page styling

--- app/factories/factory.js
+++ app/factories/factory.js
@@ -7,7 +7,7 @@ OpenGov.factory('DataFactory', function(){
 			description : 'Proyek Hambalang adalah proyek yang keren',
 			long_desc : 'We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard, because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone, and one which we intend to win.',
 			funding : 'IDR 2.3 Billion',
-			initiator : 'Jokowi',
+			initiator : 'John Smith',
 			keyword : 'hambalang',
 			location : 'jakarta',
 			latlng : '-6.549335,106.901924',
